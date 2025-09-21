d3f629f613ec600d61d0ad86ac294fff46e5a0e1
Oleksiy Beseda <beseda.odessa@gmail.com>
2025-09-21 15:45:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 #Caesar cipher
 
-text = 'Hello Zaira'
-shift = 3
+message = 'Hello Zaira'
+offset = 3
 
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    encrypted_message = ''
 
     for char in message.lower():
         if char == ' ':
-            encrypted_text += char
+            encrypted_message += char
         else:
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+            encrypted_message += alphabet[new_index]
+    print('plain message:', message)
+    print('encrypted message:', encrypted_message)
 
-# caesar(text, shift)
-caesar(text, 13)
+# (index + offset) % len(alphabet) is used to avoid index error for cases when, for example, "z" char + 3 is out of range of alphabet's possible index no.
+# message.lower() is used to prevent mistakes when Upper-case chars are used
+# encrypted_message += char = encrypted_message = encrypted_message + char
+# .find(char) - is used to find the alphabetical index no. of the message character(char)
+# alphabet can be expanded with other symbols
+# caesar(message, offset) or caesar(message, 3) are to similar ways to call function
+caesar(message, 3)
