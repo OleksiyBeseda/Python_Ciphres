70f21584cd3172e6f6ed441b3958cae985bedade
Oleksiy Beseda <beseda.odessa@gmail.com>
2025-09-21 18:00:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,43 @@ def caesar(message, offset):
 # alphabet can be expanded with other symbols
 # caesar(message, offset) or caesar(message, 3) are to similar ways to call function
 caesar(message, 3)
+
+
+#Vigenere cipher
+text = 'mrttaqrhknsw ih puggrur'    #text to be ciphred
+custom_key = 'happycoding'          #password
+
+def vigenere(message, key, direction=1):     
+    #by assigning direction=1, encryption works in normar way, if direction=-1 it is mean decryption as code works in oposite direction
+    key_index = 0     
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        # .isalpha() is a boolean used to append any non-letter character to the message
+        if not char.isalpha():      
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]       #added to avoid cases when custom_key is longer than text
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)         #third argument is absent as it is used "direction=1" by default from vigenere function
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')        
+print(f'Key: {custom_key}')               #string formatter equals to print('Key: ' + custom_key)
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
